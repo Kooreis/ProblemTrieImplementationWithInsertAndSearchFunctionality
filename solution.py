6c2ed54baebbc456ab81e4ac3dc22c0c28e2d6c8
Kooreis <kooreis.official@gmail.com>
2024-05-22 13:44:13 -0500

    This update introduces the TrieNode class. Each TrieNode object has a dictionary of its children and a boolean indicating whether it is the end of a string.

--- solution.py
+++ solution.py
@@ -1,42 +1,4 @@
-```python
 class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfString = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word):
-        current = self.root
-        for i in word:
-            node = current.children.get(i)
-            if node is None:
-                node = TrieNode()
-                current.children.update({i:node})
-            current = node
-        current.endOfString = True
-
-    def search(self, word):
-        current = self.root
-        for i in word:
-            node = current.children.get(i)
-            if node is None:
-                return False
-            current = node
-        if current.endOfString is True:
-            return True
-        return False
-
-def main():
-    trie = Trie()
-    trie.insert("apple")
-    print(trie.search("apple"))  # returns True
-    print(trie.search("app"))    # returns False
-    trie.insert("app")
-    print(trie.search("app"))    # returns True
-
-if __name__ == "__main__":
-    main()
-```
